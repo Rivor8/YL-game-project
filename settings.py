2588436267cbb8d8b3742be18c4f4f33142e50de
Sataozd <satik.o@mail.ru>
2020-01-17 19:51:01 +0300

    Added and named levels and phrases

--- settings.py
+++ settings.py
@@ -3,5 +3,7 @@ FPS = 60
 START_LEVEL = 'level1.txt'
 TILE_WIDTH, TILE_HEIGHT = 50, 50
 GAME_TITLE = 'Game'
-LEVEL_LIST = ['level1.txt', 'level2.txt', 'level3.txt']
-PHRASES = ['Глубоко... Жарко...', 'Что я?', 'sd']
+LEVEL_LIST = ['level1.txt', 'level2.txt', 'level3.txt', 'level4.txt', 'level5.txt', 'level6.txt', 'level7.txt',
+              'level8.txt', 'level9.txt']
+PHRASES = ['Ты здесь?', 'Что я?', 'Я верю, я выберусь!', 'Как это происходит?', 'Всё также жарко, но холод близок.',
+           'Я верю, а, значит, я здесь.', 'Зачем?', 'Нет дна. Холод...', 'Я хотел, но темнота рядом.', 'Я здесь.']
